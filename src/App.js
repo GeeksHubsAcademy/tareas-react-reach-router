0074d949053fa57732fd78169dbb6b15e222321f
jupegarnica <jupegarnica@gmail.com>
2019-09-18 10:58:45 +0200

    reach router installed and working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Router } from '@reach/router';
+import TodosList from './views/TodosList';
+import TodoDetail from './views/TodoDetail';
+import NotFound from './views/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <TodosList path="/" />
+        <TodoDetail path="/todo/:id" />
+        <NotFound path="*"/>
+      </Router>
+      <footer>
+        <h1>   La mejor app de tareas  </h1>
+
+      </footer>
     </div>
   );
 }
---
+++ src/views/TodosList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const TodosList = props => 'todos list'
+
+
+
+
+export default TodosList;
